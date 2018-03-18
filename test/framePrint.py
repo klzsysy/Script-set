63ba528247944446e7d479f33250b1e2d681708e
Sonny Yang <klzsysy@gmail.com>
2018-03-18 19:56:51 +0800

    add bad Apple text output demo

---
+++ test/framePrint.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# bad Apple text output demo
+# author: Sonny Yang
+
+import time
+import sys
+import os
+
+default_file = 'bad_apple.txt'
+
+
+def print_line(times, file):
+    with open(file) as f:
+        movie = []
+        frame = ''
+        for index, line in enumerate(f, 1):
+            frame += line
+            if index % 80 == 0:
+                movie.append(frame)
+                frame = ''
+
+    for frame in movie:
+        print(frame)
+        time.sleep(times)
+
+
+def main():
+    movie_file = default_file
+
+    if len(sys.argv) == 2:
+        movie_file = sys.argv[1]
+
+    if os.path.isfile(movie_file):
+        print_line(0.033, movie_file)
+    else:
+        raise FileExistsError('parameter {} not is file!'.format(movie_file))
+
+
+if __name__ == '__main__':
+    main()
